6ae095f28f7a93f402d4a64424aed4186f75ec8f
medinajorge <medinahdezjorge@gmail.com>
2022-11-07 13:32:32 +0100

    v101

--- setup.py
+++ setup.py
@@ -8,15 +8,18 @@ setup(
     packages=find_packages('tidypath'),
     package_dir={'': 'tidypath'},
     url='https://github.com/medinajorge/tidypath',
+    download_url='https://github.com/medinajorge/tidypath/archive/refs/tags/v1.0.1.tar.gz'
     description="Automatically store/load data in a tidy, efficient way.",
     long_description="Avoid spending time and lines of code creating directories, deciding filenames, etc. Tidypath automatically does it in an efficient, user-friendly way.",
     keywords=['tidy', 'project organization', 'project', 'organization', 'path', 'storage'],
     classifiers = [
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GNU Affero General Public License v3",
-        "Operating System :: OS Independent",
-        "Topic :: System :: Filesystems",
-        "Development Status :: 4 - Beta",
+        "Operating System :: OS Independent",        
+        "Development Status :: 5 - Production/Stable",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Office/Business",
+        "Intended Audience :: Science/Research",
     ],
     install_requires=[
         'numpy',
