dd264ff2d11b2205c3d3c010f13189c9a716787d
medinajorge <medinahdezjorge@gmail.com>
2022-11-07 19:13:00 +0100

    setup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='tidypath',
-    version='1.0.11',
+    version='1.0.12',
     author="Jorge Medina Hernández",
     author_email='medinahdezjorge@gmail.com',
     packages=['tidypath'],
