b61e49e6d3692eaec5c143ee4d60887100974961
medinajorge <medinahdezjorge@gmail.com>
2022-11-07 13:12:24 +0100

    __init__

--- setup.py
+++ setup.py
@@ -1,10 +1,8 @@
 from setuptools import setup, find_packages
 
-
 setup(
     name='tidypath',
-    version='0.0.1',
-    license='GNU General Public License v3.0',
+    version='1.0.0',
     author="Jorge Medina Hernández",
     author_email='medinahdezjorge@gmail.com',
     packages=find_packages('tidypath'),
@@ -13,10 +11,17 @@ setup(
     description="Automatically store/load data in a tidy, efficient way.",
     long_description="Avoid spending time and lines of code creating directories, deciding filenames, etc. Tidypath automatically does it in an efficient, user-friendly way.",
     keywords=['tidy', 'project organization', 'project', 'organization', 'path', 'storage'],
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU Affero General Public License v3",
+        "Operating System :: OS Independent",
+        "Topic :: System :: Filesystems",
+        "Development Status :: 4 - Beta",
+    ],
     install_requires=[
         'numpy',
         'pandas', 
         'matplotlib',
         'plotly'
-      ],
+    ],
 )
--- tidypath/__init__.py
+++ tidypath/__init__.py
@@ -1,19 +1,9 @@
-import sys as _sys
-import os as _os
 from . import fmt
 from . import storage
 from .decorators import savedata, savefig
 from .paths import add_arg, delete_arg, modify_arg
 
-# Set all paths refered to RootDir
-RootDir = _os.path.dirname(_os.path.dirname(__file__))
-_os.chdir(RootDir)
-_sys.path.append(RootDir)
-
-__version__ = "1.0"
-
-__all__ = ['RootDir',
-           "savedata",
+__all__ = ["savedata",
            "savefig",
            'fmt',
            'storage',
